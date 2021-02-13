eae55d24395ceabe45e4622c149dffe02e56ce31
jk-gan <kuhn96@gmail.com>
2021-02-13 18:37:11 +0800

    update styling

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,15 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen container mx-auto">
+    <div className="flex items-center justify-center h-screen container mx-auto px-5">
       <div>
-        <h1 className="text-center font-bold text-4xl mb-1">Gan Jun Kai</h1>
-        <p className="text-center font-medium text-2xl text-gray-500">Building the World's Most Advanced Education Platform at <a href="https://www.mindvalley.com">Mindvalley</a>.</p>
-        <p className="text-center text-lg text-gray-400">Elixir + Rust</p>
-        <div className="flex items-center justify-center mt-3 text-gray-300">
-          <a href="https://github.com/jk-gan"><FontAwesomeIcon className="mx-2" icon={faGithub} size="2x" /></a>
-          <a href="https://twitter.com/jk_gan"><FontAwesomeIcon className="mx-2" icon={faTwitter} size="2x" /></a>
-          <a href="https://www.linkedin.com/in/ganjk"><FontAwesomeIcon className="mx-2" icon={faLinkedin} size="2x" /></a>
+        <h1 className="text-left font-bold text-5xl mb-4">Gan Jun Kai</h1>
+        <p className="text-left font-medium text-2xl text-gray-500 mb-1">Building the World's Most Advanced Education Platform at <a href="https://www.mindvalley.com">Mindvalley</a>.</p>
+        <p className="text-left text-lg text-gray-400">Elixir + Rust</p>
+        <div className="flex items-center mt-3 text-gray-300">
+          <a href="https://github.com/jk-gan"><FontAwesomeIcon className="mr-2 hover:text-gray-400" icon={faGithub} size="2x" /></a>
+          <a href="https://twitter.com/jk_gan"><FontAwesomeIcon className="mx-2 hover:text-gray-400" icon={faTwitter} size="2x" /></a>
+          <a href="https://www.linkedin.com/in/ganjk"><FontAwesomeIcon className="mx-2 hover:text-gray-400" icon={faLinkedin} size="2x" /></a>
         </div>
       </div>
     </div>
