325aad77f85a89bafae60f7f760cc577d2070a2f
jk-gan <kuhn96@gmail.com>
2021-02-13 18:56:01 +0800

    add some colors

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
     <div className="flex items-center justify-center h-screen container mx-auto px-5">
       <div>
         <h1 className="text-left font-bold text-5xl mb-4">Gan Jun Kai</h1>
-        <p className="text-left font-medium text-2xl text-gray-500 mb-1">Building the World's Most Advanced Education Platform at <a href="https://www.mindvalley.com">Mindvalley</a>.</p>
+        <p className="text-left font-medium text-2xl text-gray-500 mb-1">Building the World's Most Advanced Education Platform at <a href="https://www.mindvalley.com" className="bg-clip-text text-transparent bg-gradient-to-r from-mv-1 viw-mv-2 to-mv-3">Mindvalley</a>.</p>
         <p className="text-left text-lg text-gray-400">Elixir + Rust</p>
         <div className="flex items-center mt-3 text-gray-300">
           <a href="https://github.com/jk-gan"><FontAwesomeIcon className="mr-2 hover:text-gray-400" icon={faGithub} size="2x" /></a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'mv-1': '#FB00FF',
+        'mv-2': '#9400FF',
+        'mv-3': '#003FFF',
+      },
+    },
   },
   variants: {
     extend: {},
