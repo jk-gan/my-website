bb35c804b3c28ee5c09540ed18238071fb929178
jk-gan <kuhn96@gmail.com>
2021-02-13 17:03:58 +0800

    update the home page

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,9 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen">
       <div>
-        <h1 className="text-center font-bold text-3xl">Gan Jun Kai</h1>
-        <h3 className="text-center text-lg">Full stack developer. Writing my own Rust web framework now.</h3>
+        <h1 className="text-center font-bold text-4xl mb-1">Gan Jun Kai</h1>
+        <p className="text-center font-medium text-2xl text-gray-500">Building the World's Most Advanced Education Platform at <a href="https://www.mindvalley.com">Mindvalley</a>.</p>
+        <p className="text-center text-lg text-gray-400">Elixir + Rust</p>
       </div>
     </div>
   )
