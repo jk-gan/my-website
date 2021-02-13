80b9249ed278e2f4a58efca2a126b0c38c30bbf7
jk-gan <kuhn96@gmail.com>
2021-02-13 19:44:13 +0800

    update home page

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,8 @@ export default function Home() {
     <div className="flex items-center justify-center h-screen container mx-auto px-5">
       <div>
         <h1 className="text-left font-bold text-5xl mb-4">Gan Jun Kai</h1>
-        <p className="text-left font-medium text-2xl text-gray-500 mb-1">Building the World's Most Advanced Education Platform at <a href="https://www.mindvalley.com" className="bg-clip-text text-transparent bg-gradient-to-r from-mv-1 viw-mv-2 to-mv-3">Mindvalley</a>.</p>
-        <p className="text-left text-lg text-gray-400">Elixir + Rust</p>
+        <p className="text-left font-medium text-2xl text-gray-500 mb-1">Building the World's Most Advanced Education Platform at <a href="https://www.mindvalley.com" className="bg-clip-text text-transparent bg-gradient-to-r from-mv-1 viw-mv-2 to-mv-3">Mindvalley</a></p>
+        <p className="text-left text-lg text-gray-400">Full stack developer · Elixir · Rust · React</p>
         <div className="flex items-center mt-3 text-gray-300">
           <a href="https://github.com/jk-gan"><FontAwesomeIcon className="mr-2 hover:text-gray-400" icon={faGithub} size="2x" /></a>
           <a href="https://twitter.com/jk_gan"><FontAwesomeIcon className="mx-2 hover:text-gray-400" icon={faTwitter} size="2x" /></a>
