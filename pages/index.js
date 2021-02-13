1d860280b392106f625283d24b90b92da538a971
jk-gan <kuhn96@gmail.com>
2021-02-13 17:55:08 +0800

    add container

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen container mx-auto">
       <div>
         <h1 className="text-center font-bold text-4xl mb-1">Gan Jun Kai</h1>
         <p className="text-center font-medium text-2xl text-gray-500">Building the World's Most Advanced Education Platform at <a href="https://www.mindvalley.com">Mindvalley</a>.</p>
