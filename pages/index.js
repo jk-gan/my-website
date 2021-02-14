08926801c9f1625264b9cfd3eff49202e53e47bf
jk-gan <kuhn96@gmail.com>
2021-02-14 15:35:05 +0800

    add animation on home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { motion } from 'framer-motion'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
@@ -7,6 +8,21 @@ export default function Home() {
   const url = "https://jk-gan.vercel.app"
   const title = "Gan Jun Kai"
   const description = "Jun Kai writes about software engineering and programming"
+
+  const fadeIn = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 }
+  }
+
+  const fadeInEaseIn = {
+    hidden: { opacity: 0 },
+    visible: { 
+      opacity: 1,
+      transition: {
+        ease: "easeIn",
+      }
+    },
+  }
 
   return (
     <>
@@ -24,20 +40,56 @@ export default function Home() {
       </Head>
       <div className="flex items-center justify-center h-screen container mx-auto px-5 2xl:w-6/12 md:w-11/12">
         <div>
-          <h1 className="text-left font-bold text-5xl mb-4">Gan Jun Kai</h1>
-          <p className="text-left font-medium text-2xl text-gray-500 mb-1">Making the web better and building the World's Most Advanced Education Platform at <a href="https://www.mindvalley.com" className="bg-clip-text text-transparent bg-gradient-to-r from-mv-1 viw-mv-2 to-mv-3">Mindvalley</a></p>
-          <p className="text-left text-lg text-gray-400">Full stack developer · Elixir · Rust · React</p>
-          <div className="flex items-center mt-3 text-gray-300 divide-x">
+          <motion.h1 
+            className="text-left font-bold text-5xl mb-3"
+            variants={fadeInEaseIn}
+            initial="hidden"
+            animate="visible"
+          >
+            Gan Jun Kai
+          </motion.h1>
+          <motion.p 
+            className="text-left font-medium text-2xl text-gray-500 mb-1"
+            variants={fadeInEaseIn}
+            initial="hidden"
+            animate="visible"
+           >
+             Making the web better and building the World's Most Advanced Education Platform at <a href="https://www.mindvalley.com" className="bg-clip-text text-transparent bg-gradient-to-r from-mv-1 viw-mv-2 to-mv-3">Mindvalley</a>
+          </motion.p>
+          <motion.p 
+            className="text-left text-lg text-gray-400"
+            variants={fadeInEaseIn}
+            initial="hidden"
+            animate="visible"
+          >
+            Full stack developer · Elixir · Rust · React
+          </motion.p>
+          <motion.div className="flex items-center mt-3 text-gray-300 divide-x" variants={fadeIn} initial="hidden" animate="visible" transition={{ delayChildren: 0.2, staggerChildren: 0.25 }}>
             <div>
-              <a href="https://github.com/jk-gan"><FontAwesomeIcon className="mr-2 hover:text-gray-400" icon={faGithub} size="1x" /></a>
-              <a href="https://twitter.com/jk_gan"><FontAwesomeIcon className="mx-2 hover:text-gray-400" icon={faTwitter} size="1x" /></a>
-              <a href="https://www.linkedin.com/in/ganjk"><FontAwesomeIcon className="ml-2 mr-4 hover:text-gray-400" icon={faLinkedin} size="1x" /></a>
+              <motion.a 
+                href="https://github.com/jk-gan"
+                variants={fadeIn}
+              >
+                <FontAwesomeIcon className="mr-2 hover:text-gray-400" icon={faGithub} size="1x" />
+              </motion.a>
+              <motion.a 
+                href="https://twitter.com/jk_gan"
+                variants={fadeIn}
+              >
+                 <FontAwesomeIcon className="mx-2 hover:text-gray-400" icon={faTwitter} size="1x" />
+              </motion.a>
+              <motion.a 
+                href="https://www.linkedin.com/in/ganjk"
+                variants={fadeIn}
+              >
+                <FontAwesomeIcon className="ml-2 mr-4 hover:text-gray-400" icon={faLinkedin} size="1x" />
+              </motion.a>
             </div>
             <div>
-              <Link href="/blog"><a className="ml-4 mr-2 hover:text-gray-400">Blog</a></Link>
-              <a className="mx-2 hover:text-gray-400" href="mailto:kuhn96@gmail.com">Contact</a>
+              <Link href="/blog"><motion.a className="ml-4 mr-2 cursor-pointer hover:text-gray-400" variants={fadeIn}>Blog</motion.a></Link>
+              <motion.a className="mx-2 hover:text-gray-400" href="mailto:kuhn96@gmail.com" variants={fadeIn}>Contact</motion.a>
             </div>
-          </div>
+          </motion.div>
         </div>
       </div>
     </>
