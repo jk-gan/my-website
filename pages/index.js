638967896d8f824914f85bdd24b3e5bb244fc842
jk-gan <kuhn96@gmail.com>
2021-02-23 23:22:47 +0800

    fix typo
    Signed-off-by: jk-gan <kuhn96@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
       <div className="flex container mx-auto mt-16 w-11/12 2xl:w-6/12 xl:w-7/12 lg:w-8/12 md:w-10/12">
         <div>
           <h1 className="text-left font-bold text-3xl mb-10">Hey, I'm Jun Kai.</h1>
-          <p className="text-left text-xl text-blueGray-800 mb-3">I'm building the world's most advanced education platform for <a className="link" href="https://mindvalley.com">Mindvalley</a>.</p>
+          <p className="text-left text-xl text-blueGray-800 mb-3">I'm building the world's most advanced education platform at <a className="link" href="https://mindvalley.com">Mindvalley</a>.</p>
           <p className="text-left text-xl text-blueGray-800 mb-3">I writes <strong>Elixir</strong> at work and <strong>Rust</strong> at my free time. Currently, I'm reading <a className="link" href="https://www.databass.dev">Database Internals</a> to learn more about database architecture.</p>
           <p className="text-left text-xl text-blueGray-800 mb-3">You can also find me on <a className="link" href="https://twitter.com/jk_gan">Twitter</a>, <a className="link" href="https://github.com/jk-gan">GitHub</a> and <a className="link" href="https://www.linkedin.com/in/ganjk">LinkedIn</a>.</p>
         </div>
